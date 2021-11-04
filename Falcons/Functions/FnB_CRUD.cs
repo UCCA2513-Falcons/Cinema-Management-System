1e3330f5426aed2f69251c035ece1273210eb396
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-11-05 00:05:18 +0800

    Add Function folder for crud

---
+++ Falcons/Functions/FnB_CRUD.cs
@@ -0,0 +1,18 @@
+﻿using Falcons.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Falcons.Functions
+{
+    public class FnB_CRUD
+    {
+        private readonly FalconsDBContext _context;
+
+        public FnB_CRUD(FalconsDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
