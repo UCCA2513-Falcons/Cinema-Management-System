a8fdd7764688a9f54c84624a22571c45d840f90c
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-11-06 22:35:25 +0800

    recover menu UI

--- Falcons/Pages/management/FnB/Products/Edit.cshtml.cs
+++ Falcons/Pages/management/FnB/Products/Edit.cshtml.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Falcons.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -19,9 +22,15 @@ namespace Falcons.Pages.management.FnB.Products
         [BindProperty]
         public Product Product { get; set; }
 
-        public EditModel(FalconsDBContext context)
+        [BindProperty]
+        public IFormFileCollection Photo { get; set; }
+
+        private IWebHostEnvironment webHostEnvironment;
+
+        public EditModel(FalconsDBContext context, IWebHostEnvironment WebHostEnvironment)
         {
             _context = context;
+            webHostEnvironment = WebHostEnvironment;
         }
 
         public async Task<IActionResult> OnGetAsync(int? id)
@@ -53,6 +62,49 @@ namespace Falcons.Pages.management.FnB.Products
             if (!ModelState.IsValid)
             {
                 return Page();
+            }
+
+            //image upload
+            string UniqueFileName = null;
+            if(Photo != null)
+            {
+                //System.DateTime CurrentTime = new DateTime();
+                //string DateFolder = CurrentTime.Year + "-" + CurrentTime.Month;
+
+                string UploadFolder = webHostEnvironment.WebRootPath + "\\assets\\img\\";
+
+                /*
+                //create the folder if not exist
+                if (!(Directory.Exists(UploadFolder)))
+                {
+                    Directory.CreateDirectory(UploadFolder);
+                }
+
+                
+                UniqueFileName = Guid.NewGuid().ToString() + "_" + Photo[0];
+                string FilePath = Path.Combine(UploadFolder, UniqueFileName);
+                using(var FileStream = new FileStream(FilePath, FileMode.Create))
+                {
+                    Photo[0].CopyTo(FileStream);
+                }*/
+
+                List<string> URLList = new List<string>();
+
+                foreach(var item in Photo)
+                {
+                    UniqueFileName = Guid.NewGuid().ToString() + "_" + item;
+                    string FilePath = Path.Combine(UploadFolder, UniqueFileName);
+                    using (var FileStream = new FileStream(FilePath, FileMode.Create))
+                    {
+                        await item.CopyToAsync(FileStream);
+                        await FileStream.FlushAsync();
+                    }
+
+                    //direct assign if the img url is the first one
+                    URLList.Add(FilePath);
+                }
+
+                Product.ImageURL = String.Join(",",URLList);
             }
 
             _context.Attach(Product).State = EntityState.Modified;
