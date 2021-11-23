1900513d1d64bb2dd704a280debd8e042b32ef38
syyyyy16 <imshenyi@yahoo.com>
2021-11-23 15:10:37 +0800

    sy

--- Falcons/Pages/management/Ticketing/Booking.cshtml.cs
+++ Falcons/Pages/management/Ticketing/Booking.cshtml.cs
@@ -6,6 +6,7 @@ using Falcons.Code;
 using Falcons.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Falcons.Pages.management.Ticketing
@@ -21,40 +22,72 @@ namespace Falcons.Pages.management.Ticketing
 
         [BindProperty]
 
-        public Booking Booking { get; set; }
+        public Booking booking { get; set; }
 
-        public List<Booking> BookingList { get; set; }
+        public IEnumerable<Booking> Booking { get; set; }
 
-        public Int32 BID { get; set; }
+        public int BID { get; set; }
 
-        public async Task<IActionResult> OnGetAsync()
+        // public List<SelectListItem> Options { get; set; }
+
+        public async Task OnGet()
         {
-            BookingList = await _context.Booking.ToListAsync();
-            return Page();
+            /* Options = await _context.Movies.Select(m =>
+             new SelectListItem
+             {
+                 Value = m.MovieID.ToString(),
+                 Text = m.MovieName.ToString()
+             }).ToListAsync();
+
+             SelectListItem initialItem = new SelectListItem
+             { Text = "Select a movie", Value = "" };*/
+
+            Booking = await _context.Booking.ToListAsync();
+
         }
 
         // add button is clicked
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+            /* if (!ModelState.IsValid)
+             {
+                 return Page();
+             }*/
 
-            _context.Booking.Add(Booking);
-            await _context.SaveChangesAsync();
+            //_context.Booking.Add(Booking);
+            //await _context.SaveChangesAsync();
 
             // await _context.booking.ToListAsync();
 
-            return Page();
+            await _context.Booking.AddAsync(booking);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("/management/Ticketing/Booking");
 
         }
 
+        /*
         public IActionResult OnPostCancel()
         {
 
             TempData["message"] = "No record added";
             return Page();
+        }*/
+
+        /*public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var bookings = await _context.Booking.FindAsync(id);
+            _context.Booking.Remove(bookings);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("/management/Ticketing/Booking");
+        }*/
+
+        public async Task<IActionResult> OnPostDelete(int ID)
+        {
+            var bookings = await _context.Booking.FindAsync(ID);
+            _context.Booking.Remove(bookings);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("/management/Ticketing/Booking");
         }
     }
 }
+
