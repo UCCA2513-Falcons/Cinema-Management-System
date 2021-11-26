d6bd192cc101ca2055d452b45b303d3a3a4a5905
ahhpeng <psh0520@1utar.my>
2021-11-26 13:28:22 +0800

    update falconsdb cointext

--- Falcons/Models/FalconsDBContext.cs
+++ Falcons/Models/FalconsDBContext.cs
@@ -22,28 +22,21 @@ namespace Falcons.Models
         public DbSet<FnBOrder> FnBOrders { get; set; }
         public DbSet<FnBOrderDetail> FnBOrderDetails { get; set; }
 
-<<<<<<< HEAD
-        //public DbSet<Employee> Employee { get; set; }
-=======
         public DbSet<Employee> Employees { get; set; }
         public DbSet<EmployeeLeave> EmployeeLeaves { get; set; }
->>>>>>> b44e8906a0743d25fdff2d328db44db6879eaf18
-
-        // ticket
 
         public DbSet<Booking> Booking { get; set; }
 
         public DbSet<BookedSeat> BookedSeats { get; set; }
 
-<<<<<<< HEAD
+
         public DbSet<Showroom> Hall { get; set; }
-=======
+
         // public DbSet<Ticket> Ticket { get; set; }
 
         // public DbSet<Seat> Seat { get; set; }
 
         //  public DbSet<Hall> Hall { get; set; }
->>>>>>> b44e8906a0743d25fdff2d328db44db6879eaf18
 
         // Movies
         public DbSet<MovieDetails> Movies { get; set; }
